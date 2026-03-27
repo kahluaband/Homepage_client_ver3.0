cf9eb55d6ad51736d28ca221d4a0308568b1c4fe
YuminPark <p353700@gmail.com>
2026-03-28 03:37:46 +0900

    [FEAT] 공유앨범 홈 페이지 구현

--- app/(kahlua)/admin/album/page.tsx
+++ app/(kahlua)/admin/album/page.tsx
@@ -1,13 +1,37 @@
 'use client';
 
-import Banner from '@/components/about/Banner';
+import AlbumBanner from '@/components/album/AlbumBanner';
 import AlbumFolder from '@/components/album/AlbumFolder';
-
 const page = () => {
   return (
-    <div className="font-pretendard relative mx-auto h-auto flex flex-col justify-center pad:w-[786px] dt:w-[1200px]">
-      <Banner />
-      <AlbumFolder type="KAHLUA" thumbnailUrl="/image/album/thumbnail_ex.jpg" />
+    <div className="w-[360px] font-pretendard relative mx-auto h-auto flex flex-col justify-center mt-20 pad:w-[786px] dt:w-[1200px] gap-[64px]">
+      <AlbumBanner type="album" />
+      <div className="flex flex-col gap-[160px] items-center justify-center pad:gap-[76px] dt:flex-row dt:gap-[254px]">
+        <div className="flex flex-col gap-8 items-center">
+          <AlbumFolder
+            type="KAHLUA"
+            thumbnailUrl="/image/album/thumbnail_ex.jpg"
+          />
+          <p className="font-pretendard text-center text-black text-[24px] font-semibold">
+            깔루아 공유 앨범
+          </p>
+          <button className="w-[172px] h-[43px] bg-yellow-main rounded-[43px] text-[24px] font-medium">
+            보러가기
+          </button>
+        </div>
+        <div className="flex flex-col gap-8 items-center">
+          <AlbumFolder
+            type="CREW"
+            thumbnailUrl="/image/album/thumbnail_ex.jpg"
+          />
+          <p className="font-pretendard text-center text-black text-[24px] font-semibold">
+            22기 공유 앨범
+          </p>
+          <button className="w-[172px] h-[43px] bg-red-main rounded-[43px] text-[24px] font-medium text-gray-0">
+            보러가기
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
